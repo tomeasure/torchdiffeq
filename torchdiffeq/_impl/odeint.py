d07e2ddbab08ae6307d7e43dc7d0e7f30b147872
Liu Shuai <957419821@qq.com>
2019-05-15 15:37:28 +0800

    把一些英文信息搞成中文
    方便阅读。

--- torchdiffeq/_impl/odeint.py
+++ torchdiffeq/_impl/odeint.py
@@ -1,3 +1,4 @@
+#! encoding="utf-8"
 from .tsit5 import Tsit5Solver
 from .dopri5 import Dopri5Solver
 from .fixed_grid import Euler, Midpoint, RK4
@@ -18,44 +19,38 @@ SOLVERS = {
 
 
 def odeint(func, y0, t, rtol=1e-7, atol=1e-9, method=None, options=None):
-    """Integrate a system of ordinary differential equations.
+    """对一个ODE系统进行积分
 
-    Solves the initial value problem for a non-stiff system of first order ODEs:
+    解决了一阶ODE非刚性系统的初值问题:
         ```
         dy/dt = func(t, y), y(t[0]) = y0
         ```
-    where y is a Tensor of any shape.
+    其中y是任意形状的张量。
 
-    Output dtypes and numerical precision are based on the dtypes of the inputs `y0`.
+    输出的dtypes和数值精度基于输入“y0”的dtypes。
 
     Args:
-        func: Function that maps a Tensor holding the state `y` and a scalar Tensor
-            `t` into a Tensor of state derivatives with respect to time.
-        y0: N-D Tensor giving starting value of `y` at time point `t[0]`. May
-            have any floating point or complex dtype.
-        t: 1-D Tensor holding a sequence of time points for which to solve for
-            `y`. The initial time point should be the first element of this sequence,
-            and each time must be larger than the previous time. May have any floating
-            point dtype. Converted to a Tensor with float64 dtype.
-        rtol: optional float64 Tensor specifying an upper bound on relative error,
-            per element of `y`.
-        atol: optional float64 Tensor specifying an upper bound on absolute error,
-            per element of `y`.
-        method: optional string indicating the integration method to use.
-        options: optional dict of configuring options for the indicated integration
-            method. Can only be provided if a `method` is explicitly set.
-        name: Optional name for this operation.
+        func: 将持有状态“y”和标量张量`t`的张量映射到关于时间（time）的状态导数的张量的函数。
+
+        y0: N-D 张量在时间点`t [0]`给出的`y`的起始值。dtype可以是浮点数或复数。
+        
+        t: 1-D Tensor，持有一系列时间点来解决“y”。初始时间点应该是此序列的第一个元素，并且每个时
+        间必须大于前一个时间。可能有任何浮点dtype。转换为float64 dtype的Tensor。
+
+        rtol: 可选的float64 Tensor，指定相对误差的上限，`y`的每个元素。
+
+        atol: 可选的float64 Tensor，指定绝对误差的上限，`y`的每个元素。
+        method: 字符串，指定积分的方法。
+        options: 可选字典，为指定的积分方法配置选项。只有在明确设置`method`时才能提供。
+        name: 可选，该步操作的名称.
 
     Returns:
-        y: Tensor, where the first dimension corresponds to different
-            time points. Contains the solved value of y for each desired time point in
-            `t`, with the initial value `y0` being the first element along the first
-            dimension.
+        y: 张量，第一维对应不同的时间点。包含了`t`中每个所需时间点的y的求解值，其中，第一维的第一个元素是初始值`y0`。
+
 
     Raises:
-        ValueError: if an invalid `method` is provided.
-        TypeError: if `options` is supplied without `method`, or if `t` or `y0` has
-            an invalid dtype.
+        ValueError: 非法的`method`.
+        TypeError: 无`method`有`options`, 或者`t`/`y0`的dtype非法。
     """
 
     tensor_input, func, y0, t = _check_inputs(func, y0, t)
@@ -63,7 +58,7 @@ def odeint(func, y0, t, rtol=1e-7, atol=1e-9, method=None, options=None):
     if options is None:
         options = {}
     elif method is None:
-        raise ValueError('cannot supply `options` without specifying `method`')
+        raise ValueError('未指定`method`，`options`无效')
 
     if method is None:
         method = 'dopri5'
